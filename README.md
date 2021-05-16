d7593a13aece44c09eb73638ea38be62c6bbb381
akmr328 <akm328@gmail.com>
2021-05-16 11:39:04 -0400

    setting up

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
-<p align="center">
-  <a href="https://www.gatsbyjs.com/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter">
-    <img alt="Gatsby" src="https://www.gatsbyjs.com/Gatsby-Monogram.svg" width="60" />
-  </a>
-</p>
-<h1 align="center">
-  Gatsby minimal starter
-</h1>
+⚡️ I'm Annie! Coding and 🍕 enthusiast | 📍 Richmond, VA | 🇺🇸 🇮🇪 Citizen | Fluent in JavaScript [ES6] | React | Redux | Node.js | Express | Postgres | Sequelize | HTML | CSS | Git ⚡
 
 ## 🚀 Quick start
 
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>Hey, I'm Annie!</title>
+      <h1>Hey, I'm Annie</h1>
+      <p>I'm Annie! I've spent the last 10 years in the Art world. I have a master's degree in Art Business and have worked as as a specialist/cataloger for Rare Books and Works on Paper. I started to understand that there was a ceiling in terms of how much I could continue to learn and grow in the industry, and felt this was a great time to pivot and find something new that was both challenging and came with more choices for the future. A friend had attended Grace Hopper and highly recommended it. I began coding by learning Python on an app and enjoyed it - I found myself exceeding my daily time goal of coding as I enjoyed the process of puzzling out answers. I was drawn to Grace Hopper by their mission in increasing representation in tech and bringing more voices to the table. I'm excited to continue my coding journey!</p>
+    </main>
+  )
+}
+
+export default AboutPage
