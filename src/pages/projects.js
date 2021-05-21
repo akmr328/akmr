7484eeb5c80ade8ae475216ae96ebb097e4024a4
akmr328 <akm328@gmail.com>
2021-05-20 23:03:15 -0400

    adds info for experience and some projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Layout from '../components/layout'
+import github from '../images/github.png'
 
 
 
@@ -8,7 +9,11 @@ const ProjectsPage = () => {
     <main id="projects">
       <title>Projects</title>
       <Layout pageTitle="Projects">
-          <h2>Novel App</h2>
+          <h3>Novel App</h3>
+          <p>A Progressive Web App for second-market book sales offering location verification to list and purchase books in your area.</p>
+          <img src={github} className="tiny-logo" /><a href="https://github.com/Novel-App/novel_app">Novel App</a>
+          <br /> 
+          <a href="https://novel-market.herokuapp.com/">Novel App</a>
       </Layout>
     </main>
   )
